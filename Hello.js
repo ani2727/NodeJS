70cfdd2e20835fc403db8cd53d98830ec71f75bc
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-03-17 10:34:23 +0530

    Create Hello.js

---
+++ Hello.js
@@ -0,0 +1,7 @@
+//code
+
+console.log("Hey there! Iam JS");
+
+//to run this on node we have two ways
+//1. npm hello.js
+//2. npm hello //because by defualt the node consider it as js file and runs only js files
