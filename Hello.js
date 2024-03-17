473485f5359c931839ff18a8885a6374c1459188
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-03-17 10:36:02 +0530

    Update Hello.js

--- Hello.js
+++ Hello.js
@@ -3,5 +3,5 @@
 console.log("Hey there! Iam JS");
 
 //to run this on node we have two ways
-//1. npm hello.js
-//2. npm hello //because by defualt the node consider it as js file and runs only js files
+//1. node hello.js
+//2. node hello //because by defualt the node consider it as js file and runs only js files
